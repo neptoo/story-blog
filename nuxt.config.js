40b12c485041faa4c055308f71aa8d124d5bd754
neptoo <neptoo57@gmail.com>
2020-01-04 19:57:13 +0800

    add header

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,14 +5,17 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    // title: process.env.npm_package_name || '',
+    title: "Storyblock + Nuxt = <3",
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      // { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: "An awesome blog bulit with Nuxt and Storyblok in 2020" }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Lato:400,700'}
     ]
   },
   /*
